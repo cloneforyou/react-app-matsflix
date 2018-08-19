0fae6f5a7f2f7369319c7c2fc64547806b08f32f
Mats Jørgensen <matsjoer@gmail.com>
2018-08-19 17:57:34 +0200

    Add scss.media-commons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import './index.scss';
+import { typograpgy } from 'scss.media-commons'
 
 const Index = () => {
   return <div>Hello React!</div>;
